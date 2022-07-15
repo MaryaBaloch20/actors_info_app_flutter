8ddaa3b69bbae12f7fb40a37ec46ccee1d3523b8
Marya Baloch <maryabaloch7@gmail.com>
2022-07-15 19:37:01 +0500

    page view and controller added

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return OnBoardingScreen();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: OnBoardingScreen(),
+    );
   }
 }
