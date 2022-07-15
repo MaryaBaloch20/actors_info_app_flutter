70241acfdaf2eedb75cfa8ab12036e7b531cc645
Marya Baloch <maryabaloch7@gmail.com>
2022-07-15 19:50:41 +0500

    Introscreens added

---
+++ lib/intro_screens/intro_screen_01.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class IntroScreen1 extends StatelessWidget {
+  const IntroScreen1({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.red[200],
+    );
+  }
+}
