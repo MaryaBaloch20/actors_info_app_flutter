c57138cfbbbde6daa10cb70a37b245b9f577ac23
Marya Baloch <maryabaloch7@gmail.com>
2022-07-15 19:36:18 +0500

    page view and controller added

--- lib/screens/onboarding_screen.dart
+++ lib/screens/onboarding_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 class OnBoardingScreen extends StatefulWidget {
   OnBoardingScreen({Key? key}) : super(key: key);
@@ -8,8 +9,36 @@ class OnBoardingScreen extends StatefulWidget {
 }
 
 class _OnBoardingScreenState extends State<OnBoardingScreen> {
+  //controller to keep track of what page we're on
+  PageController _controller = PageController();
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Stack(
+        children: [
+          //page view
+          PageView(
+            controller: _controller,
+            children: [
+              Container(
+                color: Colors.red,
+              ),
+              Container(
+                color: Colors.green,
+              ),
+              Container(
+                color: Colors.yellow,
+              ),
+            ],
+          ),
+
+          //dot indicator | wraped in container for alignment
+          Container(
+              alignment: const Alignment(0, 0.75),
+              child: SmoothPageIndicator(controller: _controller, count: 3))
+        ],
+      ),
+    );
   }
 }
